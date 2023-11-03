2045d0d653528da216837f199c3c64cc2a7504d0
tvk29052002 <khai290502@gmail.com>
2023-11-03 18:22:32 +0800

    latest

---
+++ testing.py
@@ -0,0 +1,9 @@
+import neuspell
+from neuspell import BertChecker
+
+checker = BertChecker()
+checker.from_pretrained()
+# import neuspell
+# print(neuspell.seq_modeling.downloads.CHECKPOINTS_NAMES)
+
+
